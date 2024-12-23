53b3318698eb8c8dcd95776c288211b4c6de97ad
AenuHub <jesuskane92@gmail.com>
2024-12-23 17:29:35 +0300

    create Program.cs

--- W13_DependancyInjection/Program.cs
+++ W13_DependancyInjection/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using W13_DependancyInjection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var classroom = new Classroom(
+            new Teacher { FirstName = "John", LastName = "Keating" }
+            );
+        
+        Console.WriteLine(classroom.GetTeacherInfo());
+        
+        Console.ReadKey();
+    }
+}
+  
