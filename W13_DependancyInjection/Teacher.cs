f7fb1f2cb48335c3ce496d5f16d5a10dba9b02f3
AenuHub <jesuskane92@gmail.com>
2024-12-23 17:29:51 +0300

    create Teacher.cs

--- W13_DependancyInjection/Teacher.cs
+++ W13_DependancyInjection/Teacher.cs
@@ -1,6 +1,13 @@
 ﻿namespace W13_DependancyInjection;
 
-public class Teacher
+public class Teacher : ITeacher
 {
-    
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    // returns full name
+    public string GetInfo()
+    {
+        return $"{FirstName} {LastName}";
+    }
 }
